8724ef4df65af3ebf65c7e90c93d552211db3ecb
nknkcho <srirachacho@gmail.com>
2022-05-25 03:35:14 +0900

    PR 피드백 반영 완료 [x] http method, url 상수화 [x] .then 문 -> async await으로 변경

--- src/model/article.js
+++ src/model/article.js
@@ -1,42 +1,21 @@
-// 22.05.13
-// METHOD 모듈화
-const httpMethod = {
-  get: "GET",
-  post: "POST",
-  patch: "PATCH",
-  delete: "DELETE",
-};
+//052322 HTTP METHOD, url 모듈화
+import { httpMethod, defaultUrl, status } from "./common.js";
 
-
-// 22.05.13
-// REQUEST MODULE
+//052422 REQUEST MODULE 모듈화
 async function request(path, method, body, headers = {}) {
-  const url = `http://localhost:8080/${path}`;
+  const url = defaultUrl + '/' + path;
   const options = {
     method: method,
     headers: {
-      // FIXME: GET 요청일 때 아래 헤더를 제외해야 한다. 고칠 방법 생각 필요.
-      "Content-Type": "application/json",
       ...headers,
     },
     body: JSON.stringify(body),
   };
   const res = await fetch(url, options);
-  let data = "";
-  if (res.status === 204) { // 다른 status code까지 모듈화
-    data = "NO CONTENT";
-    return data;
-  }
-  data = await res.json();
-
-  if (res.ok) {
-    return data;
-  } else {
-    throw Error(data);
-  }
+  return res;
 }
 
-// 22.05.13
+// 051322
 // 게시글 생성 request
 // URL : /articles
 function reqCreateArticle() {
@@ -52,9 +31,14 @@ function reqCreateArticle() {
     is_published: 1,
   };
 
-  request(`articles`, httpMethod.post, body)
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  try{
+    //052422 async await 도입
+    const response = await request(`articles`, httpMethod.post, body, {"Content-Type": "application/json"});
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.log(error);
+  }
 }
 
 // 22.05.13
@@ -75,9 +59,14 @@ function reqEditArticle() {
     category_id: 2,
   };
 
-  request(`articles`, httpMethod.patch, body)
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  try{
+    //052422 async await 도입
+    const response = await request(`articles`, httpMethod.patch, body, {"Content-Type": "application/json"});
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.log(error);
+  }
 }
 
 // 22.05.13
@@ -95,9 +84,14 @@ function reqDeleteArticle() {
   // DELETE request는 바디값이 없어야 하는 것이 일반적.
   // 따라서 삭제할 게시물 id는 바디가 아닌 url로 전달한다.
   // 여기서 전달하는 body 값은 API 테스트를 위한 dummy data
-  request(`articles/${article_id}`, httpMethod.delete, body)
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  try{
+    //052422 async await 도입
+    const response = await request(`articles/${article_id}`, httpMethod.delete, body, {"Content-Type": "application/json"});
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.log(error);
+  }
 }
 
 // 게시글 상세 조회 request
@@ -108,22 +102,32 @@ function reqGetArticle() {
   // dummy data
   const article_id = 157;
 
-  request(`articles/${article_id}`, httpMethod.get)
-    .then((data) => {
-      // writer: 작성자, title: 글제목, content: 글내용,
-      // views: 조회수, comments: 댓글 총 개수, likes: 좋아요 총 개수
-      const result =
-        `writer: ${data["writer"]}\n
-        title: ${data["article"].title}\n
-        content: ${data["article"].content}\n
-        views: ${data["article"].views}\n
-        comments: ${data["article"].total_comments}\n
-        likes: ${data["article"].total_likes}\n`
+  try{
+    //052422 async await 도입
+    const response = await request(`articles/${article_id}`, httpMethod.get);
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.log(error);
+  }
 
-      console.log(result);
-      // console.log(data["writer"], data["article"]);
-    })
-    .catch((error) => console.log(error));
+  //긁어올 데이터 참고하기 위해 주석처리, 추후 삭제 예정
+  // request(`articles/${article_id}`, httpMethod.get)
+  //   .then((data) => {
+  //     // writer: 작성자, title: 글제목, content: 글내용,
+  //     // views: 조회수, comments: 댓글 총 개수, likes: 좋아요 총 개수
+  //     const result =
+  //       `writer: ${data["writer"]}\n
+  //       title: ${data["article"].title}\n
+  //       content: ${data["article"].content}\n
+  //       views: ${data["article"].views}\n
+  //       comments: ${data["article"].total_comments}\n
+  //       likes: ${data["article"].total_likes}\n`
+
+  //     console.log(result);
+  //     // console.log(data["writer"], data["article"]);
+  //   })
+  //   .catch((error) => console.log(error));
 }
 
 // 22.05.13
@@ -145,50 +149,16 @@ function reqGetMyArticles() {
   // 0은 임시 발행한 게시물을, 1은 유저가 발행한 게시물을 의미한다.
   const is_published = 1;
 
-  request(`articles/my/${is_published}?${query}`, httpMethod.get)
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  try{
+    //052422 async await 도입
+    const response = await request(`articles/my/${is_published}?${query}`, httpMethod.get);
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.log(error);
+  }
 }
 
-// 22.05.13
-// 게시글 목록 조회 request
-// URL : /articles?limit&cursor&key
-function reqGetArticles() {
-  // dummy data
-  // 쿼리로 들어갈 변수들의 정보, 전체 게시글 목록 조회는 cursor pagination 형식으로 구현
-  // cursor: 조회된 목록의 첫번째 인덱스
-  // limit: cursor로부터 몇 개의 게시물을 가져올 지 결정
-  // key: 검색어
-  const query_info = {
-    cursor: 0,
-    limit: 10,
-    key: "",
-  };
-
-  // 쿼리문으로 생성
-  // TODO: Object.keys 주어진 객체의 속성 이름들을 일반적인 반복문과 동일한 순서로 순회되는 열거할 수 있는 배열로 반환
-  const query = Object.keys(query_info)
-    .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(query_info[k]))
-    .join("&");
-
-  request(`articles?${query}`, httpMethod.get)
-    .then((json) => {
-      let result = "";
-      for(let i = 0; i < json['data'].length; i++){
-        result = result + `
-          "id": ${json['data'][i].id}
-          "user_id": ${json['data'][i].user_id}
-          "title": ${json['data'][i].title}
-          "create_at": ${json['data'][i].create_at}
-          "comments": ${json['data'][i].total_comments}
-          "likes": ${json['data'][i].total_likes}
-        `;
-      }
-      // console.log(json['data']);
-      console.log(result);
-    })
-    .catch((error) => console.log(error));
-}
 
 // 22.05.13
 // 게시글 좋아요 생성 또는 취소 request
@@ -202,7 +172,12 @@ function reqLikes() {
     article_id: 156
   };
 
-  request(`articles/likes`, httpMethod.post, body)
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  try{
+    //052422 async await 도입
+    const response = await request(`articles/likes`, httpMethod.post, body, {"Content-Type": "application/json"});
+    const data = await response.json();
+    return data;
+  } catch(error){
+    console.log(error);
+  }
 }
