c0867d7c705183065d748c4f09e8f3a0217ae113
nknkcho <srirachacho@gmail.com>
2022-05-25 13:46:55 +0900

    각 경로의 목적에 대한 주석 추가

--- app.js
+++ app.js
@@ -13,50 +13,54 @@ app.use(express.static('src/view/pages'));
 app.use(express.static('src/view/styles'));
 app.use(express.static('src/assets/images'));
 
+// 052522 fetch가 데이터를 잘 불러오는지 확인하기 위한 테스트용 
 app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/model/test.html'));
 })
-
+// 홈화면
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/home.html'));
 });
-
+// 로그인
 app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/login.html'));
 });
-
+// 비밀번호 찾기
 app.get('/pwd', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/find_pwd.html'));
 });
-
+// 회원가입 1 : 메일 주소와 비밀번호 입력
 app.get('/signup', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/signup.html'));
 });
-
+// 회원가입 2 : 닉네임 설정
 app.get('/nickname', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/signup_profile.html'));
 });
-
+// 회원가입 3 : 서비스 분야 설정
 app.get('/field', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/signup_field.html'));
 });
-
+// 회원가입 4 : 회원가입 완료시 환영 문구 (홈으로 가기, 로그인 하기)
 app.get('/welcome', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/signup_welcome.html'));
 });
-
+// (로그인 완료 후) 내 게시물 조회
 app.get('/myposting', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/mypage_posting.html'));
 });
-
+// (로그인 완료 후) 내 계정 정보 조회/변경/탈퇴
 app.get('/account', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/mypage_account_setting.html'));
 });
-
+// 게시글 목록 조회
 app.get('/articles', function(req, res) {
   res.sendFile(path.join(__dirname, '/src/view/pages/articles.html'));
 });
-
+// 게시물 상세 조회
+app.get('/article', function(req, res) {
+  res.sendFile(path.join(__dirname, '/src/view/pages/article.html'));
+});
 
 
 app.listen(PORT, function () {
