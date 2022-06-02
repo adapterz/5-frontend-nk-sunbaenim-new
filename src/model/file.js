817398a1289eca3fe13ef374a6fdfa795f445f13
nknkcho <srirachacho@gmail.com>
2022-06-02 16:53:02 +0900

    프로필 사진 업로드 및 조회 기능 추가

--- src/model/file.js
+++ src/model/file.js
@@ -1,30 +1,33 @@
+import { server } from "./common.js";
+export { reqCreateProfileImage, reqCreateArticleImage };
+
 // 22.05.14
 // 프로필 이미지 등록 request
 // URL : /files
-async function reqCreateProfileImage(){
-  const file = 'dummy.jpg';
-  const formData = new FormData();
-  formData.append('avatar', file);
-
-  const user_id = 51
-  const url = `http://localhost:8080/files/${user_id}`;
+async function reqCreateProfileImage(userId, formData){
   const options = {
     method: "POST",
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    },
     body: formData,
   };
+  console.log(options)
 
-  fetch(url, options)
-  .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  try{
+    const response = await fetch(`${server}/files/${userId}`, options);
+    if(response.status === 204){
+      console.log("Profile image updated")
+      return
+    } else {
+      throw new Error();
+    }
+  } catch(error) {
+    console.log(error);
+  }
 }
 
 // 22.05.14
 // 게시물 이미지 등록 request
 // URL : /files
-async function reqCreateProfileImage(){
+async function reqCreateArticleImage(){
   const file = 'dummy.jpg';
   const formData = new FormData();
   formData.append('article_files', file);
