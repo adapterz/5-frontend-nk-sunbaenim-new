9a460b34ac5994cb639da3139d43657364a0d711
nknkcho <srirachacho@gmail.com>
2022-06-02 16:54:05 +0900

    회원가입, 닉네임 등록 기능 구현

--- src/model/user.js
+++ src/model/user.js
@@ -3,38 +3,23 @@
 //2. 단, req.body, req.params, req.query로 보내는 변수들은 스네이크로 유지한다. 이유는 백엔드와 변수 형식이 다르면 자칫 타이핑 에러가 날 가능성도 있을 것 같아서.
 
 //052322 HTTP METHOD, url 모듈화
-import { httpMethod, defaultUrl} from "./common.js";
-
-//052422 http 메소드, url, status 코드 모듈화로 리팩토링
-async function request(path, method, body, headers = {}) {
-  const url = defaultUrl + "/" + path;
-  const options = {
-    method: method,
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      ...headers,
-    },
-    body: JSON.stringify(body),
-  };
-  const res = await fetch(url, options);
-  return res;
-}
-
+import { httpMethod, request } from "./common.js";
+export { reqSignUp, reqCreateNickname };
 //회원가입 request
 //URL : /users/signup
-async function reqSignUp() {
-  //dummy data
+async function reqSignUp(email, pwd, pwd_check) {
   const body = {
-    email: "fetch_test18@gmail.com",
-    pwd: "qwerQWER!",
-    pwd_check: "qwerQWER!",
+    email: email,
+    pwd: pwd,
+    pwd_check: pwd_check,
   };
-
   //052522 async/await 문으로 바꿔준다
   try {
-    const response = await request(`users/signup`, httpMethod.post, body);
+    const response = await request(`users/signup`, httpMethod.post, body, {
+      "Content-Type": "application/json",
+    });
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -42,23 +27,30 @@ async function reqSignUp() {
 }
 
 //내 닉네임 등록 request
-//URL : /users/:users_id/ninkname
-async function reqCreateNickname() {
-  //dummy data
-  //22.05.12 찾아보니 api 주소를 상황에 맞게 유동적으로 바꿔주어야 할 경우(req.params)에는 React의 props를 사용하면 된다고 한다. 일단 user_id는 고정값으로 둔다.
-  const user_id = 50;
+//서버 URL : /users/:users_id/ninkname
+async function reqCreateNickname(user_id, nickname) {
   const body = {
-    nickname: "fetchcatcher",
+    nickname: nickname,
   };
 
   try {
     const response = await request(
       `users/${user_id}/nickname`,
       httpMethod.post,
-      body
+      body,
+      {
+        "Content-Type": "application/json",
+      }
     );
-    const data = await response.json();
-    return data;
+
+    if (response.status === 409) {
+      return 409;
+    } else if (response.status === 400) {
+      return 400;
+    } else if (response.status === 204) {
+      const data = await response.json();
+      return data;
+    }
   } catch (error) {
     console.log(error);
   }
@@ -82,29 +74,6 @@ async function reqCreateField() {
     );
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-//로그인 request
-//URL : /users/login
-// document.getElementById('login').addEventListener('submit', reqLogin);
-document.getElementById("event").addEventListener("click", reqLogin);
-async function reqLogin() {
-  //dummy data
-  const body = {
-    email: "fetch_test16@gmail.com",
-    pwd: "qwerQWER!qwer",
-  };
-
-  try {
-    const response = await request(`users/login`, httpMethod.post, body);
-    const data = await response.json();
-    if (response.status === 200) {
-      console.log(data);
-      response.sendRedirect();
-    }
   } catch (error) {
     console.log(error);
   }
